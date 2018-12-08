84f846621c6d1bcf7f3fb5420c4813edbf972fa5
naresh0274 <pnarendharreddy92@gmail.com>
2018-12-08 15:46:20 -0500

    changed to JunitTest.java file

--- src/JunitTest.java
+++ src/JunitTest.java
@@ -7,6 +7,7 @@ public class JunitTest {
 	@Test
 	public void test() {
 		Junit J1=new Junit();
+		System.out.println("Hello to unittesting ");
 		int result=J1.sum(5,2);
 		assertEquals(7,result);
 		
