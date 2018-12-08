bcfa1b3f4c39cbc6361d5ab0cac595874977fd76
naresh0274 <pnarendharreddy92@gmail.com>
2018-12-08 15:48:53 -0500

    fixed code in Demo

--- src/JunitTest.java
+++ src/JunitTest.java
@@ -7,7 +7,7 @@ public class JunitTest {
 	@Test
 	public void test() {
 		Junit J1=new Junit();
-		System.out.println("Hello to unittesting ");
+		System.out.println("Hello ");
 		int result=J1.sum(5,2);
 		assertEquals(7,result);
 		
