614b75f7e8fbeef216e0da13c65cf1b80fb27091
naresh0274 <pnarendharreddy92@gmail.com>
2018-12-08 15:55:17 -0500

    adding new file and modified

--- src/JunitTest.java
+++ src/JunitTest.java
@@ -7,9 +7,10 @@ public class JunitTest {
 	@Test
 	public void test() {
 		Junit J1=new Junit();
-		System.out.println("Hello ");
+		System.out.println("Hello world ");
 		int result=J1.sum(5,2);
 		assertEquals(7,result);
+		System.out.println("Hello ");
 		
 	}
 
